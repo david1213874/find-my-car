3f8032e4563df511a249b65c3c25e4625a2eea2d
david1213874 <75838919+david1213874@users.noreply.github.com>
2020-12-13 21:35:54 +0800

    Update map.js

--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,7 +1,6 @@
 // miniprogram/pages/map/map.js
-const devicesId = "657062777" // 填写在OneNet上获得的devicesId 形式就是一串数字 例子:9939133
-const api_key = "WlTQD2RhkW3DG=kslg5cXfiZmUk=" // 填写在OneNet上的 api-key 例子: VeFI0HZ44Qn5dZO14AuLbWSlSlI=
-
+const devicesId = "657054151" 
+const api_key = "lJOULFScfN5Xvn2Df4Lg4t3G9Pc=" 
 Page({
   data: {
     longitude_you:116.39,
@@ -13,47 +12,12 @@ Page({
       url: '../start/start',
     })
   },
-  /*
- get_your_loc:function (e) {
-   var that =this
-   wx.getLocation({
-     success:function(res){
-       //console.log(res)
-       that.setData({
-        longitude_you:res.longitude,
-        latitude_you:res.latitude,
-        markers:[{
-          id:0,
-          longitude:res.longitude,
-          latitude:res.latitude,
-          title :"您的位置",
-          iconPath:"../../image/you.png",
-          width:30,
-          height:30
-      },
-        {id:1,
-          longitude:116.39,
-          latitude:39.90,
-          title :"车的位置",
-          iconPath:"../../image/car.png",
-          width:30,
-          height:30
-        }]
-       })
-     }
-   })
-  //console.log("get_you_loc")
-  },
-  */
-  
   onLoad: function(loc_you,e) {
    var that =this
    wx.getLocation({
      success:function(res){
        //console.log(res)
        that.setData({
-        longitude_you:res.longitude,
-        latitude_you:res.latitude,
         markers:[{
           id:0,
           longitude:res.longitude,
@@ -62,53 +26,137 @@ Page({
           iconPath:"../../image/you.png",
           width:30,
           height:30
-      },
-        {id:1,
-          longitude:116.39,
-          latitude:39.90,
-          title :"车的位置",
-          iconPath:"../../image/car.png",
-          width:30,
-          height:30
-        }]
+      }]
        })
      }
    })
-  //console.log("get_you_loc")
-   },search:function(){
+   },
+
+getlongitude_car:function(){
+  var longitude_car=116.39
+  wx.request({
+    url: 'https://api.heclouds.com/devices/657054151/datastreams/longitude',
+    header: {
+     'content-type': 'application/json',
+     'api-key': api_key,
+   }, method:"GET",
+   success: (res) => {
+     this.setData({
+       longitude_car:res.data.data.current_value
+     })
+   }
+  })
+  return this.data.longitude_car
+},
+getlatitude_car:function(){
+    var latitude_car=39.90
+  var that=this
+  wx.request({
+    url: 'https://api.heclouds.com/devices/657054151/datastreams/latitude',
+    header: {
+     'content-type': 'application/json',
+     'api-key': api_key,
+   }, method:"GET",
+   success: (res) => {
+    this.setData({
+      latitude_car:res.data.data.current_value
+    })
+   }
+  })
+  return this.data.latitude_car
+},
+
+
+
+
+
+   search:function(){
     var that =this
     var longitude_you=this.data.markers[0].longitude;
     var latitude_you=this.data.markers[0].latitude;
+    //var longitude_car=this.getlongitude_car()
+    //console.log(longitude_car)
+    that.setData({
+      markers:[{ id:0,
+       longitude:longitude_you,
+       latitude:latitude_you,
+       title :"您的位置",
+       iconPath:"../../image/you.png",
+       width:30,
+       height:30},
+      {
+        id:1,
+        longitude:this.getlongitude_car(),
+        latitude:this.getlatitude_car(),
+        title :"车的位置",
+        iconPath:"../../image/car.png",
+        width:30,
+        height:30
+      }]
+     })
+    /*
      wx.request({
-       url: 'https://api.heclouds.com/devices/657062777/datastreams/location',
+       url: 'https://api.heclouds.com/devices/657054151/datastreams/longitude',
        header: {
         'content-type': 'application/json',
         'api-key': api_key,
       }, method:"GET",
       success: (res) => {
           //console.log(res)
-          //console.log(res.data.data.current_value)
-         that.setData({
-           markers:[{ id:0,
-            longitude:longitude_you,
-            latitude:latitude_you,
-            title :"您的位置",
-            iconPath:"../../image/you.png",
-            width:30,
-            height:30},
-           {
-             id:1,
-             longitude:res.data.data.current_value.lon,
-             latitude:res.data.data.current_value.lat,
-             title :"车的位置",
-             iconPath:"../../image/car.png",
+          
+          console.log(res.data.data.current_value)
+          that.setData({
+            longitude_car:res.data.data.current_value,
+            markers:[{ id:0,
+             longitude:longitude_you,
+             latitude:latitude_you,
+             title :"您的位置",
+             iconPath:"../../image/you.png",
              width:30,
-             height:30
-           }]
-          })
+             height:30},
+            {
+              id:1,
+              longitude:res.data.data.current_value,
+              latitude:latitude_car,
+              title :"车的位置",
+              iconPath:"../../image/car.png",
+              width:30,
+              height:30
+            }]
+           })
+           console.log(longitude_car)
+           
       }
      })
-        
-    console.log("search")
+     wx.request({
+       url: 'https://api.heclouds.com/devices/657054151/datastreams/latitude',
+       header: {
+        'content-type': 'application/json',
+        'api-key': api_key,
+      }, method:"GET",
+      success: (res) =>{
+        console.log(res.data.data.current_value)
+        that.setData({
+          markers:[{ id:0,
+           longitude:longitude_you,
+           latitude:latitude_you,
+           title :"您的位置",
+           iconPath:"../../image/you.png",
+           width:30,
+           height:30},
+          {
+            id:1,
+            longitude:longitude_car,
+            latitude:res.data.data.current_valu,
+            title :"车的位置",
+            iconPath:"../../image/car.png",
+            width:30,
+            height:30
+          }]
+         })
+      }
+     }) 
+     */
+    //console.log("search")
   },
 })
